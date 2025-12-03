435c7ad3fac74da4d28e7a102dacea0d87b1e3be
JeremyAyza <88175896+JeremyAyza@users.noreply.github.com>
2025-12-03 16:43:44 -0500

    add counter component

--- src/components/shared/contador.tsx
+++ src/components/shared/contador.tsx
@@ -1,25 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-export function Contador() {
+const Contador: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
-    // Crea el primer script dinámicamente
-    const script1 = document.createElement("script");
-    script1.src = "https://www.freevisitorcounters.com/auth.php?id=90566c52eb2a847b5f03f6ae293dfdf9cf1e926e";
-    script1.async = true;
-    document?.getElementById("contador")?.appendChild(script1);
+    if (!containerRef.current) return;
 
-    // Crea el segundo script dinámicamente
+    // Limpia el contenedor (si React rerenderiza)
+    containerRef.current.innerHTML = `
+      <a href="https://www.free-counters.org/">powered by Free-Counters.org</a>
+    `;
+
+    // Script 1
+    const script1 = document.createElement("script");
+    script1.type = "text/javascript";
+    script1.src =
+      "https://www.freevisitorcounters.com/auth.php?id=90566c52eb2a847b5f03f6ae293dfdf9cf1e926e";
+    script1.async = true;
+    containerRef.current.appendChild(script1);
+
+    // Script 2
     const script2 = document.createElement("script");
-    script2.src = "https://www.freevisitorcounters.com/en/home/counter/1452531/t/2";
+    script2.type = "text/javascript";
+    script2.src =
+      "https://www.freevisitorcounters.com/en/home/counter/1452531/t/2";
     script2.async = true;
-    document?.getElementById("contador")?.appendChild(script2);
+    containerRef.current.appendChild(script2);
   }, []);
 
-  return (
-    <div id="contador">
-      <a href="https://www.free-counters.org/">powered by Free-Counters.org</a>
-    </div>
-  );
-}
+  return <div ref={containerRef}></div>;
+};
 
-
+export default Contador;
